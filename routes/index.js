4c7a8f05bde71a35828cfdd5353a077deaf25f09
jyh <yhjin@hhsoft.co.kr>
2023-05-11 15:31:32 +0900

    1

--- routes/index.js
+++ routes/index.js
@@ -3,38 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render("index")
+  res.render("index", { message: null })
 });
 router.get("/join", function (req, res) {
-  res.render("join")
+  res.render("join", { message: null })
 })
-router.post("/api/join", function (req, res) {
+router.post("/api/join", async function (req, res) {
   console.log(req.body)
   var [rows] = await connection.query("select * from user where id=?", [req.body.id])
   if (rows.length == 0) { // 중복된 아이디가 없어서 회원가입이 가능함
     var [rows] = await connection.query("insert into user(id,name,email,password) values(?,?,?,?)",
       [req.body.id, req.body.name, req.body.email, req.body.password])
 
+    res.redirect("/")
   }
-  else {
-
-
+  else {  // 이미 가입되어 있는 아이디
+    res.render("join", { message: "이미 가입된 아이디입니다." })
   }
-
-
-  // connection.query("select * from user where id=?", [req.body.id], function (err, rows) {
-  //   console.log(err, rows)
-  //   if (rows.length == 0) { // 중복된 아이디가 없어서 회원가입이 가능함
-  //     connection.query("insert into user(id,name,email,password) values(?,?,?,?)",
-  //       [req.body.id, req.body.name, req.body.email, req.body.password], function (err, rows) {
-
-  //       })
-  //   }
-  //   else { //이미 가입되어 있는 아이디
-
-  //   }
-
-  // })
 })
+router.post("/api/login", async function (req, res) {
+  console.log(req.body)
+  var [rows] = await connection.query("select * from user where id=? and password=?",
+    [req.body.id, req.body.password])
+  console.log(rows)
+  if (rows.length == 0) { // 로그인 실패 - 아이디 또는 패스워드가 틀렸을경우
+    res.render("index", { message: "아이디 또는 패스워드가 틀렸습니다." })
+  }
+  else { //로그인 성공
+    res.redirect("/main")
+  }
+})
+
 
 module.exports = router;
